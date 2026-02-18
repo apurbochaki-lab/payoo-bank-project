b2b9b9cde02613604da22d762112ecec21925fdb
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-02-18 22:18:39 +0600

    Cashout section only UI done

---
+++ scripts/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('cashout-btn').addEventListener('click', function() {
+    //1. Get agent number, validate & convert to number
+    //2. Get amount, validate & convert to number
+    //3. Get Current Balance, validate, convert to number
+    //4. Calculate new balance
+    //5. Get pin & verify
+    //6. if true:::> alert> set balance
+    //7. if false:::> error alert> return
+});
